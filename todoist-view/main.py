8de0941a54d826e391c13d946ac0200808b3cf0f
Keith W. Davis <kdavis10@nd.edu>
2023-08-15 16:36:34 -0400

    reading projects. so, functional.

--- todoist-view/main.py
+++ todoist-view/main.py
@@ -1,11 +1,15 @@
+from todoist_api.api import TodoistAPI
+# from todoist_api.api_async import TodoistAPIAsync
 import typer
+from typing_extensions import Annotated
+from rich import print
 
 
-def main(name: str, lastname: str, formal: bool = False):
-    if formal:
-        print(f" Good day M. {name} {lastname}.")
-    else:
-        print(f"Hello {name} {lastname}")
+def main(api_key: Annotated[str,
+         typer.Argument(envvar="POETRY_TODOIST_API_KEY")]):
+    api = TodoistAPI(api_key)
+    projects = api.get_projects()
+    print(projects)
 
 
 if __name__ == "__main__":
