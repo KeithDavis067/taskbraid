0f727f3d4b6e98c721c76cdd8ab70280872cd1a6
Keith W. Davis <kdavis10@nd.edu>
2023-08-16 15:27:08 -0400

    changing todoist-view to app with commands.

--- todoist-view/main.py
+++ todoist-view/main.py
@@ -2,11 +2,12 @@ from todoist_api.api import TodoistAPI
 # from todoist_api.api_async import TodoistAPIAsync
 import typer
 from typing_extensions import Annotated
-from rich import print
+from rich import print, inspect
+
+app = typer.Typer()
+state = {"api": None}
 
 PROJECT_KEYS = ["color",
-                "comment_count",
-                "color",
                 "comment_count",
                 "id",
                 "is_favorite",
@@ -18,6 +19,25 @@ PROJECT_KEYS = ["color",
                 "parent_id",
                 "url",
                 "view_style"]
+
+TASK_KEYS = [
+    "id",
+    "content",
+    "description",
+    "comment_count",
+    "is_completed",
+    "order",
+    "priority",
+    "project_id",
+    "labels",
+    "due",
+    "section_id",
+    "parent_id",
+    "creator_id",
+    "created_at",
+    "assignee_id",
+    "assigner_id",
+    "url"]
 
 # Project(
 #     color='charcoal',
@@ -33,22 +53,44 @@ PROJECT_KEYS = ["color",
 #     url='https://todoist.com/showProject?id=2307271342',
 #     view_style='list'
 # )
+#
 
 
-def show_project(project, keys=None):
-    """Print an easier to read version of a project."""
-    if keys is None:
-        print(project["name",
+# def show_item(item, style="minimal"):
+#     """Select better printing for items."""
+#     collected = {}
+#
+#     if style.lower() in ["min", "minimal"]:
+#         keys = ["name", "parent_id", "color", ]
+#
+#     for key in keys:
+#
+@app.command()
+def show(itemkind: str):
+    api = state["api"]
+    print(f"Showing: {itemkind.lower()}")
+    match itemkind.lower():
+        case "projects":
+            result = api.get_projects()
+
+        case "tasks":
+            result = api.get_tasks()
+
+    print(result)
 
 
+@app.command()
+def create(itemkind: str):
+    api = state["api"]
+    print(f"Adding: {itemkind}")
 
-def main(api_key: Annotated[str,
-         typer.Argument(envvar="POETRY_TODOIST_API_KEY")]):
-    api=TodoistAPI(api_key)
-    projects=api.get_projects()
-    for project in projects:
-        print(project)
+
+@app.callback()
+def main(api_key:
+         Annotated[str, typer.Argument(envvar="POETRY_TODOIST_API_KEY")]):
+
+    state["api"] = TodoistAPI(api_key)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
